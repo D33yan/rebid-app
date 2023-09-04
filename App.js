e6dc6e14a1597930a5350a03d69add6295d8dc88
Deeyan <nnajidivin2006@gmail.com>
2023-09-04 09:29:57 -0700

    app context.js created

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StackNavigation } from "./screens/stack-navigation";
-import { NavigationContainer } from "@react-navigation/native";
+
 import { AppProvider } from "./config/app-context";
+import { AppNav } from "./screens/AppNav";
 export default function App() {
   return (
    <AppProvider>
-       <NavigationContainer>
-      <StackNavigation/>
-    </NavigationContainer>
+       <AppNav/>
    </AppProvider>
   )
 }
--- config/app-context.js
+++ config/app-context.js
@@ -4,9 +4,19 @@ const AppContext = createContext()
 
 function AppProvider({children}){
     const [UID,setUserUID] = useState('trigger');
+    const [userToken,setUserToken] = useState(null)
+    const [isLoading,setUserLoading] = useState(false);
+
+    const login = () =>{
+        
+    }
+
+    const logout = () =>{}
+
+    const isLoggedIn = () =>{}
 
     return (
-        <AppContext.Provider value={{UID}}>
+        <AppContext.Provider value={{UID,userToken,isLoading,login,logout}}>
             {children}
         </AppContext.Provider>
     )
